0b8cbb84fc03968297f486f1dbc88cc3681c2965
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-03-15 13:49:13 +0900

    first commit

--- Splash/ViewController.swift
+++ Splash/ViewController.swift
@@ -2,24 +2,83 @@
 //  ViewController.swift
 //  Splash
 //
-//  Created by Gwinyai on 2/7/2018.
-//  Copyright © 2018 Gwinyai. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let twitterImage = UIImageView(image: UIImage(named: "twitter")!)
+    
+    let splashView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        splashView.backgroundColor = UIColor(red: 0/255, green: 172/255, blue: 237/255, alpha: 1.0)
+        
+        view.addSubview(splashView)
+        
+        splashView.frame = CGRect(x:0, y:0, width: view.bounds.width, height: view.bounds.height)
+        
+        twitterImage.contentMode = .scaleAspectFit
+        
+        splashView.addSubview(twitterImage)
+        
+        twitterImage.frame = CGRect(x: splashView.frame.midX - 35, y: splashView.frame.midY - 35, width: 70, height: 70)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            
+            self.scaleDownAnimation()
+            
+        }
+        scaleDownAnimation()
+        
+        
     }
-
+    
+    func scaleDownAnimation() {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            
+            self.twitterImage.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+            
+        }) { (success) in
+            
+            self.scaleUpAnimation()
+            
+        }
+        
+    }
+    
+    func scaleUpAnimation() {
+        
+        UIView.animate(withDuration: 0.15, delay: 0.1, options: .curveEaseIn, animations: {
+            
+            self.twitterImage.transform = CGAffineTransform(scaleX: 5, y: 5)
+            
+        }) { (success) in
+            
+            self.removeSplashScreen()
+            
+            
+        }
+        
+    }
+    
+    func removeSplashScreen () {
+        
+        splashView.removeFromSuperview()
+        
+    }
+    
+    
 
 }
 
